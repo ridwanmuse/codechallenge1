56413350c72ea2241271c6a116445d5922a1944c
your name”git add .git config --global user.name ridwan <104872709+ridwanmuse@users.noreply.github.com>
2022-10-12 12:11:20 -0500

    “done”

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {"Bob", "Alphonse", "Lily", "Marcus","Jess"};
+        System.out.println(names[0]);
+
+
+        int ages[] = {28, 30, 25, 22, 20};
+        int index =0;
+        while (index < ages.length) {
+            System.out.println(ages[index]);
+        index++;
+        }
+        }
+        }
